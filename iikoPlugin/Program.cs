02db373538e90bce27ba58ec9f20f69b29740387
Martivis <vanek-non@rambler.ru>
2023-11-12 02:54:27 +0300

    Configure ServiceProvider

--- iikoPlugin/Program.cs
+++ iikoPlugin/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace iikoPlugin
+﻿using Microsoft.Extensions.DependencyInjection;
+using Resto.Front.Api;
+using Resto.Front.Api.Data.Brd;
+using Resto.Front.Api.Data.Common;
+using System;
+
+namespace iikoPlugin
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            HostApplicationBuilder builder = new Host.CreateApplicationBuilder(args);
+            var services = new ServiceCollection();
+            services.AddSingleton<IDataProvider, MockDataProvider>();
+            services.AddTransient<IObserver<EntityChangedEventArgs<IReserve>>, ReserveEventsLogger>();
+            services.AddTransient<IFrontPlugin, MockPlugin>();
+
+            var provider = services.BuildServiceProvider();
+
+            var plugin = provider.GetService<IFrontPlugin>();
         }
     }
 }
